d0079b665a9babaa193baac5672e801f2d7b7c27
Bamathi G <bamathi07@gmail.com>
2026-09-22 23:33:21 +0530

    Time: 10 ms (26.52%), Space: 46.6 MB (95.73%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                set.remove(num);
+            } else {
+                set.add(num);
+            }
+        }
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}
